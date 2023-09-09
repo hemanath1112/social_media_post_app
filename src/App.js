4b632dc3f7174e7ac73bdf8fc3c6fdf2bee22e1c
Hemanath Chakkaravarthi <114560429+hemanath1112@users.noreply.github.com>
2023-09-09 11:45:01 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import About from "./Componnets/About";
+import Footer from "./Componnets/Footer";
+import Header from "./Componnets/Header";
+import Home from "./Componnets/Home";
+import Missing from "./Componnets/Missing";
+import Nav from "./Componnets/Nav";
+import NewPost from "./Componnets/NewPost";
+import PostPage from "./Componnets/PostPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Home />
+      <NewPost />
+      <PostPage />
+      <About />
+      <Missing />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
